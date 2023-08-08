ff6912c950cab0162075896206389849c4d5b96d
Esteban Fernandez <estebangfernandeza@gmail.com>
2023-08-07 21:51:12 -0400

    csrf replace with simple sintax

--- src/main/java/com/efernandez/rossano/bean/SecurityBeans.java
+++ src/main/java/com/efernandez/rossano/bean/SecurityBeans.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +28,7 @@ public class SecurityBeans {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth //.anyRequest().permitAll());
                         //Public endpoints
                         .requestMatchers(
